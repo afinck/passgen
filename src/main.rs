dbadf9334b58bb16a6f7b112d720a407aa948f2e
Andreas Finck <andreas.finck@icloud.com>
2025-06-07 20:53:50 +0200

    linting

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
-use clap::Parser; //
-use rand::{thread_rng, Rng}; // Importiert die Thread-spezifische Zufallszahlengenerator-Bibliothek
 use arboard::Clipboard; // Importiert die Wayland-spezifische Clipboard-Bibliothek
+use clap::Parser; //
+use rand::{Rng, thread_rng}; // Importiert die Thread-spezifische Zufallszahlengenerator-Bibliothek
 
 #[derive(Parser)]
 #[command(version, about="Generates a random 12 character simple password", long_about = None)] // Read from `Cargo.toml`
 struct Cli {
-    #[arg(short = 'n', long = "numbers", help= "number of characters", default_value_t = 12)]
+    #[arg(
+        short = 'n', 
+        long = "numbers", 
+        help= "number of characters", 
+        default_value_t = 12
+    )]
     characters: u8,
-    #[arg(short = 'x', long = "complex", help= "add special characters", default_value_t = false)]
+    #[arg(
+        short = 'x', 
+        long = "complex", 
+        help= "add special characters", 
+        default_value_t = false
+    )]
     complex: bool,
-    #[arg(short = 'c', long = "copy", help= "copy to clipboard", default_value_t = false)]
+    #[arg(
+        short = 'c', 
+        long = "copy", 
+        help= "copy to clipboard", 
+        default_value_t = false
+    )]
     copy: bool
 }
 
@@ -19,7 +34,8 @@ fn generate_password(characters: u8, complex: bool) -> String {
     let mut rng: rand::prelude::ThreadRng = thread_rng();
     let mut chars = Vec::new();
 
-    chars.extend_from_slice(b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_+!=?");
+    chars
+        .extend_from_slice(b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_+!=?");
 
     if complex {
         chars.extend_from_slice("!§$%&/()=?*+#-_.:,;<>|@".as_bytes());
@@ -39,7 +55,9 @@ fn generate_password(characters: u8, complex: bool) -> String {
 fn copy_to_clipboard(password: String) {
     // Diese Funktion kopiert den Inhalt in die Zwischenablage und gibt eine Nachricht aus
     let mut clipboard = Clipboard::new().unwrap();
-    clipboard.set_text(password).expect("Fehler beim Kopieren in die Zwischenablage");
+    clipboard
+        .set_text(password)
+        .expect("Fehler beim Kopieren in die Zwischenablage");
     let content = clipboard.get_text().expect("Fehler beim Lesen aus der Zwischenablage");
     println!("copied to clipboard: {}", content);
 }
